847b3449bc2aea085baed8255a13912191079d3f
Diane Corriette <dcorriette@gmail.com>
2024-10-06 14:41:40 +0100

    first commit - installed Django and first app Core

---
+++ stream/urls.py
@@ -0,0 +1,14 @@
+"""
+URL configuration for stream project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
