b515f46f551e1baa3fc04797683cdc0806d5f7a1
Diane Corriette <dcorriette@gmail.com>
2024-10-06 15:31:16 +0100

    Add static files to folders assets, css, js etc

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # Use the render function to return an HTTP response
+    return render(request, 'index.html', {})
