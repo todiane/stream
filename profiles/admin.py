5e048aaee28c1cc53e0fbb6d7c96b507ecb33240
Diane Corriette <dcorriette@gmail.com>
2024-10-15 11:24:05 +0100

    Change admin in Profiles app to include name of site in admin area

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "Stream English Administration"
+admin.site.site_title = "Stream English Admin Portal"
+admin.site.index_title = "Welcome to Stream English Admin Portal"
