6dfe2c6ab0bba0fcd50f1429ee17b0efb46a300e
Diane Corriette <dcorriette@gmail.com>
2024-10-17 21:38:40 +0100

    Changes made to correct earlier mistakes

--- courses/views.py
+++ courses/views.py
@@ -28,8 +28,48 @@ def course_detail_view(request, course_id=None, *args, **kwarg):
     }
     return render(request, "courses/detail.html", context)
 
-
 def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs):
+    print(course_id, lesson_id)
+    lesson_obj = services.get_lesson_detail(
+        course_id=course_id,
+        lesson_id=lesson_id
+    )
+    if lesson_obj is None:
+        raise Http404
+    
+    email_id_exists = request.session.get('email_id')
+    if lesson_obj.requires_email and not email_id_exists:
+        print(request.path)
+        request.session['next_url'] = request.path
+        return render(request, "courses/email-required.html", {})
+    
+    # Get all lessons for the current course
+    lessons_queryset = services.get_course_lessons(lesson_obj.course)
+    
+    template_name = "courses/lesson-coming-soon.html"
+    context = {
+        "object": lesson_obj,
+        "lessons_queryset": lessons_queryset
+    }
+    
+    if not lesson_obj.is_coming_soon and lesson_obj.has_video:
+        template_name = "courses/lesson.html"
+        
+        if lesson_obj.youtube_url:
+            # For YouTube videos
+            video_id = lesson_obj.youtube_url.split('v=')[-1]
+            context['video_embed'] = f'https://www.youtube.com/embed/{video_id}'
+        elif lesson_obj.video:
+            # For Cloudinary videos
+            video_embed_html = helpers.get_cloudinary_video_object(
+                lesson_obj, 
+                field_name='video',
+                as_html=True,
+                width=1250
+            )
+            context['video_embed'] = video_embed_html
+    
+    return render(request, template_name, context)
     print(course_id, lesson_id)
     lesson_obj = services.get_lesson_detail(
         course_id=course_id,
