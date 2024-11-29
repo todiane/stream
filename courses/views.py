dc84b5d63c3c07eb774db1dfb911c68c1d91f88c
Diane Corriette <dcorriette@gmail.com>
2024-11-29 07:49:32 +0000

    removed duplicate code from courses/views file

--- courses/views.py
+++ courses/views.py
@@ -44,6 +44,7 @@ def course_detail_view(request, course_id=None, *args, **kwarg):
     }
     return render(request, "courses/detail.html", context)
 
+
 def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs):
     print(course_id, lesson_id)
     lesson_obj = services.get_lesson_detail(
@@ -86,61 +87,26 @@ def lesson_detail_view(request, course_id=None, lesson_id=None, *args, **kwargs)
             context['video_embed'] = video_embed_html
     
     return render(request, template_name, context)
-    print(course_id, lesson_id)
-    lesson_obj = services.get_lesson_detail(
-        course_id=course_id,
-        lesson_id=lesson_id
-    )
-    if lesson_obj is None:
-        raise Http404
-    
-    email_id_exists = request.session.get('email_id')
-    if lesson_obj.requires_email and not email_id_exists:
-        print(request.path)
-        request.session['next_url'] = request.path
-        return render(request, "courses/email-required.html", {})
-    
-    # Get all lessons for the current course
-    lessons_queryset = services.get_course_lessons(lesson_obj.course)
-    
-    template_name = "courses/lesson-coming-soon.html"
-    context = {
-        "object": lesson_obj,
-        "lessons_queryset": lessons_queryset  # Add this line
-    }
-    
-    if not lesson_obj.is_coming_soon and lesson_obj.has_video:
-        """
-        Lesson is published
-        Video is available
-        go forward
-        """
-        template_name = "courses/lesson.html"
-        video_embed_html = helpers.get_cloudinary_video_object(
-            lesson_obj, 
-            field_name='video',
-            as_html=True,
-            width=1250
-        )
-        context['video_embed'] = video_embed_html
-    
-    return render(request, template_name, context)
 
 
 def htmx_nav_menu(request):
     return render(request, 'courses/htmx/nav_menu.html')
 
+
 def htmx_video_modal(request):
     return render(request, 'courses/htmx/video_modal.html')
 
+
 def htmx_booking_form(request):
     return render(request, 'courses/htmx/booking_form.html')
+
 
 def htmx_latest_courses(request):
     latest_courses = services.get_publish_courses()[:3]
     return render(request, 'courses/htmx/latest_courses.html', {
         'latest_courses': latest_courses
     })
+
 
 @login_required
 def htmx_submit_booking(request):
