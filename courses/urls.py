ef0876f5d37deaa59f17c6f6a8c79f2a4e9e1dfc
Diane Corriette <dcorriette@gmail.com>
2024-12-02 10:18:00 +0000

    Update url structure to use course title instead of project id to be more seo friendly

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,12 @@
+# courses/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'courses'
 
-
 urlpatterns = [
     path('', views.course_list_view, name='course_list'),
     path('booking-form/', views.booking_form_view, name='booking_form'),
-    path('<slug:course_id>/', views.course_detail_view, name='course_detail'),
-    path('<slug:course_id>/lessons/<slug:lesson_id>/', views.lesson_detail_view, name='lesson_detail'),
+    path('<slug:course_slug>/', views.course_detail_view, name='course_detail'),
+    path('<slug:course_slug>/lessons/<slug:lesson_slug>/', views.lesson_detail_view, name='lesson_detail'),
 ]
