40d02f96ea8a9be86ff6988ab52575b68724fb36
Diane Corriette <dcorriette@gmail.com>
2024-12-18 08:12:12 +0000

    Update courses model and views to remove error in production

--- courses/services.py
+++ courses/services.py
@@ -1,31 +1,28 @@
 # courses/services.py
-
-from .models import Course, Lesson, PublishStatus
+from .models import Course, Lesson
 
 
 def get_publish_courses():
-    return Course.objects.filter(status=PublishStatus.PUBLISHED)
+    return Course.objects.filter(status="publish")
+
 
 def get_course_detail(course_slug=None):
     if course_slug is None:
         return None
     obj = None
     try:
-        obj = Course.objects.get(
-            status=PublishStatus.PUBLISHED,
-            public_id=course_slug
-        )
+        obj = Course.objects.get(status="publish", public_id=course_slug)
     except:
         pass
     return obj
+
 
 def get_course_lessons(course_obj=None):
     lessons = Lesson.objects.none()
     if not isinstance(course_obj, Course):
         return lessons
     lessons = course_obj.lesson_set.filter(
-        course__status=PublishStatus.PUBLISHED,
-        status__in=[PublishStatus.PUBLISHED, PublishStatus.COMING_SOON]
+        course__status="publish", status__in=["publish", "soon"]
     )
     return lessons
 
@@ -37,9 +34,9 @@ def get_lesson_detail(course_slug=None, lesson_slug=None):
     try:
         obj = Lesson.objects.get(
             course__public_id=course_slug,
-            course__status=PublishStatus.PUBLISHED,
-            status__in=[PublishStatus.PUBLISHED, PublishStatus.COMING_SOON],
-            public_id=lesson_slug
+            course__status="publish",
+            status__in=["publish", "soon"],
+            public_id=lesson_slug,
         )
     except Exception as e:
         print("lesson detail", e)
