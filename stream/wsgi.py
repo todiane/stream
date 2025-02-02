062ac388744d0c5503a14e4e9ea9e46d34ab19a5
Diane Corriette <dcorriette@gmail.com>
2025-02-02 18:55:09 +0000

    Update wsgi.py file

--- stream/wsgi.py
+++ stream/wsgi.py
@@ -1,13 +1,30 @@
+"""
+WSGI config for stream project.
+"""
 import os
 import sys
 
-# Add your Django project directory to the Python path
-sys.path.insert(0, "/home/str3a3eng24/public_html")
+# Set the path to your virtualenv
+VIRTUALENV_PATH = '/home/str3a3eng24/virtualenv/stream/3.10'
+if VIRTUALENV_PATH not in sys.path:
+    sys.path.insert(0, VIRTUALENV_PATH)
 
-# Set Django settings module
-os.environ["DJANGO_SETTINGS_MODULE"] = "stream.settings"
+# Add site-packages from virtualenv
+site_packages = os.path.join(VIRTUALENV_PATH, 'lib', 'python3.10', 'site-packages')
+if site_packages not in sys.path:
+    sys.path.insert(0, site_packages)
 
-# Import and create WSGI application
+# Add the Django project directory to the Python path
+project_path = '/home/str3a3eng24/stream'
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
+# Add the directory containing the project to the Python path
+parent_path = '/home/str3a3eng24'
+if parent_path not in sys.path:
+    sys.path.insert(0, parent_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stream.settings")
+
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
