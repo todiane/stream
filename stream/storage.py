2be1333fd9a5b1deadcaa81b16ac6cecdd95fcd7
Diane Corriette <131492009+todiane@users.noreply.github.com>
2026-02-20 23:30:26 +0000

    Update storage.py file to work with secure_storage folder

--- stream/storage.py
+++ stream/storage.py
@@ -1,38 +1,34 @@
 # stream/storage.py
+
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import os
 
+
 class SecureFileStorage(FileSystemStorage):
     def __init__(self):
         secure_root = os.path.join(settings.MEDIA_ROOT, "secure_downloads")
-        super().__init__(location=secure_root)
+        super().__init__(
+            location=secure_root,
+            base_url=settings.MEDIA_URL + "secure_downloads/",
+        )
 
-    def get_valid_name(self, name):
-        name = super().get_valid_name(name)
-        return name.replace("public/", "")
 
 class PublicMediaStorage(FileSystemStorage):
     def __init__(self):
         public_root = os.path.join(settings.MEDIA_ROOT, "public")
         super().__init__(
             location=public_root,
-            base_url=settings.MEDIA_URL + 'public/'
+            base_url=settings.MEDIA_URL + "public/",
         )
-
-    def get_valid_name(self, name):
-        name = super().get_valid_name(name)
-        return name.replace("public/", "")
 
     def url(self, name):
         url = super().url(name)
-        if not url.startswith(settings.MEDIA_URL):
-            url = settings.MEDIA_URL.rstrip('/') + '/public/' + name.lstrip('/')
-        # Add cache busting
         from django.utils.timezone import now
+
         timestamp = int(now().timestamp())
         return f"{url}?v={timestamp}"
 
-# Create instances
+
 secure_storage = SecureFileStorage()
 public_storage = PublicMediaStorage()
